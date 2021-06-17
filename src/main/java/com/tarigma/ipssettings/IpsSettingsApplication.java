37c6bf021d8b8a21457aa8c9c55a0e5a9b354fae
Aaron Grant <aaron.grant@tarigma.com>
2021-06-17 10:26:40 -0500

    Add Spring Initializr generated files

---
+++ src/main/java/com/tarigma/ipssettings/IpsSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.tarigma.ipssettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpsSettingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarigma/ipssettings/IpsSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarigma.ipssettings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsSettingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
