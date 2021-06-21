81b7d53bd2d71c1b7955788e2c8e186d5e9cf9fa
Aaron Grant <aaron.grant@tarigma.com>
2021-06-21 16:48:24 -0500

    Write output to ignored directory

--- src/main/java/com/tarigma/ipssettings/IpsSettingsBootstrapper.java
+++ src/main/java/com/tarigma/ipssettings/IpsSettingsBootstrapper.java
@@ -61,7 +61,7 @@ public class IpsSettingsBootstrapper implements ApplicationListener<ContextRefre
 
             // write to output
             String outputFileName = input.getFileName().toString().concat(".output.xml");
-            Path outputDir = input.getParent().resolveSibling("output");
+            Path outputDir = Path.of(System.getProperty("user.dir"), "output");
             Files.writeString(outputDir.resolve(outputFileName), xmlContent);
         } catch (Exception e) {
             LOG.error("failed to convert: {}", input, e);
