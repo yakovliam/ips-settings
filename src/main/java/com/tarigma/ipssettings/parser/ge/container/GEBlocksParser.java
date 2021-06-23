664164d9499ab2d4da6755954e732732883de6dc
Aaron Grant <aaron.grant@tarigma.com>
2021-06-23 17:42:36 -0500

    Create GEBlocksParser.java

---
+++ src/main/java/com/tarigma/ipssettings/parser/ge/container/GEBlocksParser.java
@@ -0,0 +1,17 @@
+package com.tarigma.ipssettings.parser.ge.container;
+
+import com.tarigma.ipssettings.model.Block;
+import com.tarigma.ipssettings.parser.container.BlocksParser;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GEBlocksParser implements BlocksParser {
+
+	@Override
+	public List<Block> parse(List<String> lines) {
+		// TODO Auto-generated method stub
+		return Collections.emptyList();
+	}
+
+}
